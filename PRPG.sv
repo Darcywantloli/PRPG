92bbe6f2b3ef11cd931c48e761e348707737cc8c
Darcywantloli <101007135+Darcywantloli@users.noreply.github.com>
2022-06-11 19:33:06 +0800

    Update PRPG.sv

--- PRPG.sv
+++ PRPG.sv
@@ -1,20 +1,45 @@
-module PRPG (
-           input  clk,          // 1-bit clock
-           input  rst,          // 1-bit clear Active-high synchronous
-           output reg[3:1] out  // 3-bit result
-       );
+module Pseudo_Random_Pattern_Generator (clk,Load);
+
+input clk;
+input Load;
+output [2:0] Din;
+output [2:0] Q_out;
+output Q1, Q2, Q3;
+
+reg [2:0] Din;
+reg [2:0] Q_out;
+reg Q1, Q2, Q3;
+reg temp;
 
 initial
-    out = 3;
+begin
+    temp = 0;
+    Q1 = 1;
+    Q2 = 0;
+    Q3 = 0;
+    Q_out = 3'b001;
+    Din = 3'b111;
+end
 
-always@ (posedge clk) begin
-    if(rst)
-        out <= 4;
-    else begin
-        // out[3] <= out[2];
-        // out[2] <= out[1];
-        // out[1] <= out[1] ^ out[3];
-        out <= {out[2:1], out[3]^out[1]};
+always@ (posedge clk or posedge Load)
+begin
+    if(Load)
+    begin
+        Q1 = Din[0];
+        Q2 = Din[1];
+        Q3 = Din[2];
     end
+
+    else
+    begin
+        temp = Q1;
+        Q1 = Q1 ^ Q3;
+        Q3 = Q2;
+        Q2 = temp;
+    end
+
+    Q_out[0] = Q1;
+    Q_out[1] = Q2;
+    Q_out[2] = Q3;
 end
 endmodule
